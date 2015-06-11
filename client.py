e9c1ecb6917f45fbebb41ebe47f17347b0e2e8a9
Zuchis <liquimaster@gmail.com>
2015-06-11 20:22:58 -0300

    Added UDP support, but is has limitations

--- client.py
+++ client.py
@@ -11,44 +11,75 @@ import time
 
 #print 'Socket Created'
 
-if (len(sys.argv) != 4):
-    print("Please enter the following parameters in order:\n The ip adress, the port and the maximum number of messages")
+if (len(sys.argv) != 5):
+    print("Please enter the following parameters in order:\n The ip adress, the port, the maximum number of messages, and the type of the Transfer Protocol (TCP or UDP")
     sys.exit()
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 msg = 1000
 msgMax = int(sys.argv[3]) + 100
-lol = 10485760
+lol = 50
 times = []
 f = open("output.txt","w")
 #connect to the server
 #s.connect((host , port))
 #print 'Socket Connected to ' + host + ' on port ' + str(port)
 
-while(msg != msgMax):
-    msgSent = str(msg)
-    try :
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((HOST , PORT))
-        print 'Socket Connected to ' + HOST + ' on port ' + str(PORT)
-        s.send(msgSent) # Send the number of messages to be received
-    except socket.error:
-        #Send failed
-        print 'Send failed'
-        break;
+if (sys.argv[4] == "TCP"):
+	while(msg != msgMax):
+	    msgSent = str(msg)
+	    try :
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect((HOST , PORT))
+		print 'Socket Connected to ' + HOST + ' on port ' + str(PORT)
+		s.send(msgSent) # Send the number of messages to be received
+	    except socket.error:
+		#Send failed
+		print 'Send failed'
+		break;
 
-    print "Now Receiving %d messages from Server:" % msg
-    start = time.time()
-    for it in range(0,msg):
-        try:
-            reply = s.recv(lol)
-        except socket.error:
-            print "Message %d not received" % (it)
-    end = time.time()
-    t = end-start
-    print("Finished receiving %d messages in %lf seconds\n") %(msg,t)
-    times.append(t)
-    avrg = str(msg) + "    " + str(t) + "\n"
-    f.write(avrg)
-    msg += 100
-print (times)
+	    print "Now Receiving %d messages from Server:" % msg
+	    start = time.time()
+	    for it in range(0,msg):
+		try:
+		    reply = s.recv(lol)
+		except socket.error:
+		    print "Message %d not received" % (it)
+	    end = time.time()
+	    t = end-start
+	    print("Finished receiving %d messages in %lf seconds\n") %(msg,t)
+	    times.append(t)
+	    avrg = str(msg) + "    " + str(t) + "\n"
+	    f.write(avrg)
+	    msg += 100
+	print (times)
+else:
+	while (msg != msgMax):
+	    it = msg
+	    n_bytes = lol * msg
+	    msgSent = str(msg)
+	    try :
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		s.sendto(msgSent,(HOST,PORT)) # Send the number of messages to be received
+	    except socket.error:
+		#Send failed
+		print ('Send failed')
+		break;
+
+	    print ("Now Receiving %d messages from Server:") % (msg)
+	    start = time.time()
+	    #while(it > 0):
+		#it -= 1
+	    try:
+	        d = s.recvfrom(n_bytes)
+	    except socket.error:
+	        print "Message %d not received" % (it)
+	    end = time.time()
+	    t = end-start
+	    print("Finished receiving %d messages in %lf seconds\n") %(msg,t)
+	    times.append(t)
+	    avrg = str(msg) + "    " + str(t) + "\n"
+	    f.write(avrg)
+	    msg += 100
+	    s.close()
+	print (times)
--- server.py
+++ server.py
@@ -1,19 +1,23 @@
 import socket
 import sys
+from threading import Thread
 from thread import *
 
-if (len(sys.argv) != 3):
-    print("Please enter the following parameters in order:\n The ip adress and the port")
+if (len(sys.argv) != 4):
+    print("Please enter the following parameters in order:\n The ip adress, the port and the type of the Transfer Protocol (TCP or UDP)")
     sys.exit()
 HOST = sys.argv[1]
 PORT = int(sys.argv[2])
 answer = "a"
-lol = 10485760
-for i in range(0,lol):
+lol = 50
+for i in range(0,lol-1):
     answer += 'a'
 
 # Socket Creation
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+if(sys.argv[3] == "TCP"):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+else:
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 #First argument: type of network (in this case, IPv4), second argument: type of transfer protocol (in this case, TCP)
 print 'Socket created'
 
@@ -25,8 +29,9 @@ except socket.error , msg:
     sys.exit()
 
 print 'Socket bind complete'
-s.listen(10) #Awaits for connections in the binded host and port
-print 'Socket now listening on %s / %d' % (HOST,PORT)
+if (sys.argv[3] == "TCP"):
+    s.listen(10) #Awaits for connections in the binded host and port
+    print 'Socket now listening on %s / %d' % (HOST,PORT)
 
 def clientthread(conn,addr): # This is the thread for each client
     try:
@@ -45,10 +50,25 @@ def clientthread(conn,addr): # This is the thread for each client
     conn.close()
     #print "Connection ended with %s:%s" % addr
 
-while 1:
-    #wait to accept a connection
-    conn, addr = s.accept()
-    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    start_new_thread(clientthread,(conn,addr))
+def clientUDP(msg,addr):
+    print("Now sending %d messages to %s / %d") %(msg,addr[0],addr[1])
+    while(msg > 0):
+	msg -= 1
+	s.sendto(answer, addr)
 
+if(sys.argv[3] == "UDP"):
+	print("The server is on at the adress: %s / %d") %(HOST,PORT)
+
+if (sys.argv[3] == "TCP"):
+	while 1:
+		    #wait to accept a connection
+		    conn, addr = s.accept()
+		    #print 'Connected with ' + addr[0] + ':' + str(addr[1])
+		    start_new_thread(clientthread,(conn,addr))
+else:
+    while 1:
+	    msg, addr = s.recvfrom(1024)
+	    msg = int(msg)
+	    t = Thread(target=clientUDP,args=(msg,addr))
+	    t.start() 
 s.close()
